549dc0529165fe18fea22777c72eb6e0e7cfa8d1
Chorong Shin <chorongstellashin@gmail.com>
2022-05-17 17:49:38 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,131 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    totalCost: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivery: [
+      {
+        deliveredDate: 'August 8, 2020',
+        deliveryMethod: ''
+      }
+    ],
+    purchaseItems: [
+      {
+        product1: [
+          {
+            productType: 'book',
+            details: [
+              {
+                title: 'JavaScript for impatient programmers',
+                author: 'Rauschemayer, Dr. Axel',
+                price: '$31.55',
+                returnedBy: 'September 7, 2020'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    totalCost: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    delivery: [
+      {
+        deliveredDate: 'July 20, 2020',
+        deliveryMethod: 'Your package was delivered. It was handed directly to a resident'
+      }
+    ],
+    purchasedItems: [
+      {
+        product1: [
+          {
+            productType: 'book',
+            details: [
+              {
+                title: 'The Timeless Way of Building',
+                author: 'Alexander, Christopher',
+                price: '$41.33',
+                returnedBy: 'August 19, 2020'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    totalCost: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    delivery: [
+      {
+        deliveredDate: 'July 7, 2020',
+        deliveryMethod: 'Your package was delivered. It was handed directly to a resident'
+      }
+    ],
+    purchasedItems: [
+      {
+        purchase1: [
+          {
+            productType: 'electronic device',
+            details: [
+              {
+                title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Virbration Features. No Driver and No Lag-Gamecube Adapter',
+                author: '',
+                price: '$15.98',
+                returnedBy: 'August 5, 2020'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    totalCost: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivery: [
+      {
+        deliveredDate: 'July 5, 2020',
+        deliveryMethod: ''
+      }
+    ],
+    purchaseItems: [
+      {
+        purchase1: [
+          {
+            productType: 'electronic device',
+            details: [
+              {
+                title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+                author: '',
+                price: '$94.95',
+                returnedBy: 'August 4, 2020'
+              }
+            ]
+          }
+        ],
+        purchaset2: [
+          {
+            productType: 'book',
+            details: [
+              {
+                title: 'The Art of Sql',
+                author: 'Faroult, Stephane',
+                price: '$33.99',
+                returnedBy: 'August 4, 2020'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
