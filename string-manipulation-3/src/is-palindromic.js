70020b34719e0670768dbd0aa34eb4229f631951
Chorong Shin <chorongstellashin@gmail.com>
2022-05-21 23:38:34 +0000

    In process.

---
+++ string-manipulation-3/src/is-palindromic.js
@@ -0,0 +1,18 @@
+/* exported isPalindromic */
+
+function isPalindromic(string) {
+  var reversed = string.split('').reverse().join('');
+  for (var i = 0; i < string.length; i++) {
+    if (string.charAt(i) !== reversed.charAt(i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// if (string === reversed) {
+//   return true;
+// }
+// return false;
+
+isPalindromic('sam I am');
