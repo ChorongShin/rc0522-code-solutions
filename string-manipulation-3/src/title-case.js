512faa8e61ce872512a855b84ef9fb5a45950d95
Chorong Shin <chorongstellashin@gmail.com>
2022-05-25 16:58:00 +0000

    Working on the last challenge.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -7,23 +7,64 @@
 // if the second part of the word is hypenated, capitalize it
 
 function titleCase(title) {
-  var words = title.toLowerCase().split(' ');
+  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
+  var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
+  var wordSeparators = /([ :–—-])/;
 
-  var minor = ['a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'not', 'and', 'but', 'or'];
-  // var firstWord = ['a', 'an', 'the'];
-  var array = [];
-  for (var i = 0; i < words.length; i++) {
-    if (words[i] === 'javascript' || words[i] === 'javascript:') {
-      array.push(words[i][0].toUpperCase() + words[i].slice(1, 4).toLowerCase() + words[i][4].toUpperCase() + words[i].slice(5).toLowerCase());
-    } else if (words[i] === 'api') {
-      array.push(words[i].toUpperCase());
-    } else if (minor.includes(words[i])) {
-      array.push(words[i].toLowerCase());
-    } else if (words[i].includes('-')) {
-      array.push(words[i].split('-').map(e => e.charAt(0).toUpperCase() + e.slice(1)).join('-'));
-    } else {
-      array.push(words[i][0].toUpperCase() + words[i].substr(1).toLowerCase());
-    }
-  }
-  return array.join(' ');
+  return title.split(wordSeparators)
+    .map(function (current, index, array) {
+      if (
+        /* Check for small words */
+        current.search(smallWords) > -1 &&
+        /* Skip first and last word */
+        index !== 0 &&
+        index !== array.length - 1 &&
+        /* Ignore title end and subtitle start */
+        array[index - 3] !== ':' &&
+        array[index + 1] !== ':' &&
+        /* Ignore small words that start a hyphenated phrase */
+        (array[index + 1] !== '-' ||
+          (array[index - 1] === '-' && array[index + 1] === '-'))
+      ) {
+        return current.toLowerCase();
+      }
+
+      /* Ignore intentional capitalization */
+      if (current.substr(1).search(/[A-Z]|\../) > -1) {
+        return current;
+      }
+
+      /* Ignore URLs */
+      if (array[index + 1] === ':' && array[index + 2] !== '') {
+        return current;
+      }
+
+      /* Capitalize the first letter */
+      return current.replace(alphanumericPattern, function (match) {
+        return match.toUpperCase();
+      });
+    })
+    .join('');
 }
+
+// function titleCase(title) {
+//   var words = title.toLowerCase().split(' ');
+
+//   var minor = ['a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'not', 'and', 'but', 'or'];
+//   // var firstWord = ['a', 'an', 'the'];
+//   var array = [];
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i] === 'javascript' || words[i] === 'javascript:') {
+//       array.push(words[i][0].toUpperCase() + words[i].slice(1, 4).toLowerCase() + words[i][4].toUpperCase() + words[i].slice(5).toLowerCase());
+//     } else if (words[i] === 'api') {
+//       array.push(words[i].toUpperCase());
+//     } else if (minor.includes(words[i])) {
+//       array.push(words[i].toLowerCase());
+//     } else if (words[i].includes('-')) {
+//       array.push(words[i].split('-').map(e => e.charAt(0).toUpperCase() + e.slice(1)).join('-'));
+//     } else {
+//       array.push(words[i][0].toUpperCase() + words[i].substr(1).toLowerCase());
+//     }
+//   }
+//   return array.join(' ');
+// }
