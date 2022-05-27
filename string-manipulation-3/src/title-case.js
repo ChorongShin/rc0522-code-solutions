d150d3c9ec9407415824f3b28e953cca1ab29be4
Chorong Shin <chorongstellashin@gmail.com>
2022-05-27 17:05:51 +0000

    in process.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -6,42 +6,83 @@
 // if not, capitalize the first word of the title and of any subtitle
 // if the second part of the word is hypenated, capitalize it
 
-function titleCase(title) {
-  var words = title.toLowerCase().split(' ');
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
-  var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
+// var isColon = false;
 
-  return words.map(function (currentWord, index, array) {
+// function titleCase(title) {
+//   var words = title.toLowerCase().split(' ');
+//   var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
+// var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
 
-    if (currentWord.includes('javascript')) {
-      currentWord = currentWord[0].toUpperCase() + currentWord.slice(1, 4).toLowerCase() + currentWord[4].toUpperCase() + currentWord.slice(5).toLowerCase();
-    }
-    if (currentWord === 'api') {
-      currentWord = currentWord.toUpperCase();
-    }
+//   return words.map(function (currentWord, index, array) {
 
-    if (currentWord === 'node.js') {
-      currentWord = currentWord[0].toUpperCase() + currentWord.substr(1).toLowerCase();
-    }
+//     if (currentWord.includes('javascript')) {
+//       return currentWord[0].toUpperCase() + currentWord.slice(1, 4).toLowerCase() + currentWord[4].toUpperCase() + currentWord.slice(5).toLowerCase();
+//     }
+//     if (currentWord === 'api') {
+//       currentWord = currentWord.toUpperCase();
+//     }
 
-    if (currentWord.includes('-')) {
-      return currentWord.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('-');
-    }
+//     if (currentWord.includes('-')) {
+//       return currentWord.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('-');
+//     }
 
-    if (currentWord.search(smallWords) > -1 && index !== 0 && index !== array.length - 1) {
-      return currentWord.toLowerCase();
-    }
+//     if (currentWord.search(smallWords) > -1 && index !== 0 && currentWord.length <= 3) {
+//       return currentWord.toLowerCase();
+//     }
 
-    // if () {
-    //   currentWord += array[index + 1][0].toUpperCase() + array[index + 1].slice(1).toLowerCase();
-    // }
+//     var colon = currentWord.includes(':');
+//     if (colon) {
+//       // var includeColon = currentWord[currentWord.length -1];
+//       // index = currentWord.indexOf(includeColon)
+//       index = array[array.indexOf(currentWord) + 1];
+//       array.splice(index, 1);
+//       return currentWord.charAt(0).toUpperCase() + currentWord.substr(1).toLowerCase() + ' ' + index.charAt(0).toUpperCase() + index.substr(1).toLowerCase();
+//     }
 
-    return currentWord.replace(alphanumericPattern, function (match) {
-      return match.toUpperCase();
-    });
-  })
-    .join(' ');
-}
+//     isColon = true;
+//     if (isColon) {
+//       isColon = false;
+//       return currentWord[0].toUpperCase() + currentWord.substr(1).toLowerCase();
+//     }
+
+//   }).join(' ');
+// }
+
+// var isColon = false;
+
+// function titleCase(title) {
+//   var words = title.toLowerCase().split(' ');
+//   var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
+//   var alphanumericPattern = /([A-Za-z0-9\u00C0-\u00FF])/;
+
+//   return words.map(function (currentWord, index, array) {
+//     isColon = true;
+//     if (isColon) {
+//       isColon = false;
+//       console.log(currentWord[0].toUpperCase() + currentWord.substr(1).toLowerCase());
+//     }
+
+//     if (currentWord.includes('javascript')) {
+//       currentWord = currentWord[0].toUpperCase() + currentWord.slice(1, 4).toLowerCase() + currentWord[4].toUpperCase() + currentWord.slice(5).toLowerCase();
+//     }
+//     if (currentWord === 'api') {
+//       currentWord = currentWord.toUpperCase();
+//     }
+
+//     if (currentWord.includes('-')) {
+//       return currentWord.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('-');
+//     }
+
+//     if (currentWord.search(smallWords) > -1 && index !== 0 && index !== array.length - 1) {
+//       return currentWord.toLowerCase();
+//     }
+
+//     return currentWord.replace(alphanumericPattern, function (match) {
+//       return match.toUpperCase();
+//     });
+//   })
+//     .join(' ');
+// }
 
 // function titleCase(title) {
 //   var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|v.?|vs.?|via)$/i;
