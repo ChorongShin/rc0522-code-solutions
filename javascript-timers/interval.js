81ddaf3279f1883d747949f4baf55b49f6ca4d5f
Chorong Shin <chorongstellashin@gmail.com>
2022-06-08 18:16:54 +0000

    Added clearInterval

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,13 +1,16 @@
-const $countdownDisplay = document.querySelector('.countdown-display');
+var $countdownDisplay = document.querySelector('.countdown-display');
 
-setInterval(countDown, 1000);
-
+var intervalID = null;
 var count = 4;
+
 function countDown() {
   count--;
   if (count > 0) {
     $countdownDisplay.textContent = count;
   } else {
     $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalID);
   }
 }
+
+intervalID = setInterval(countDown, 1000);
