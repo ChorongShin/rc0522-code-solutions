60d378f25d30ae3da617baaf9e1c13dbb0b6f027
Chorong Shin <chorongstellashin@gmail.com>
2022-06-09 22:14:05 +0000

    Completed

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -13,3 +13,22 @@ xhr.addEventListener('load', function () {
   }
 });
 xhr.send();
+
+// Instructor's code
+// var $userList = document.querySelector('#user-list');
+
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+// xhr.responseType = 'json';
+// xhr.addEventListener('load', function () {
+//   console.log(xhr.status);
+//   console.log(xhr.response);
+// var users = xhr.response;
+//   for (var i = 0; i < users.length; i++) {
+//     var user = users[i];
+//     var $li = document.createElement('li');
+//     $li.textcontent = user.name;
+//     $userList.appendChild($li);
+//   }
+// });
+// xhr.send();
