65bd08b4d9c1746890011a07f39c7d429693918e
Chorong Shin <chorongstellashin@gmail.com>
2022-06-29 18:06:36 +0000

    Completed node-require exercise.

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,15 +3,19 @@ const subtract = require('./subtract');
 const multiply = require('./multiply');
 const divide = require('./divide');
 
+const num1 = parseInt(process.argv[2], 10);
+const num2 = parseInt(process.argv[4], 10);
+const operation = process.argv[3];
+
 let result;
-if (process.argv[3] === 'plus') {
-  result = add(parseInt(process.argv[2], 10), parseInt(process.argv[4], 10));
-} else if (process.argv[3] === 'minus') {
-  result = subtract(parseInt(process.argv[2], 10), parseInt(process.argv[4], 10));
-} else if (process.argv[3] === 'over') {
-  result = divide(parseInt(process.argv[2], 10), parseInt(process.argv[4], 10));
-} else if (process.argv[3] === 'times') {
-  result = multiply(parseInt(process.argv[2], 10), parseInt(process.argv[4], 10));
+if (operation === 'plus') {
+  result = add(num1, num2);
+} else if (operation === 'minus') {
+  result = subtract(num1, num2);
+} else if (operation === 'over') {
+  result = divide(num1, num2);
+} else if (operation === 'times') {
+  result = multiply(num1, num2);
 }
 
 console.log('Result:', result);
