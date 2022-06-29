ed014e5da7648fc6f989c4b04e739912de2db313
Chorong Shin <chorongstellashin@gmail.com>
2022-06-29 22:22:42 +0000

    Added other code solution

--- the-event-loop/interval.js
+++ the-event-loop/interval.js
@@ -13,3 +13,14 @@ function countDown() {
 }
 
 intervalID = setInterval(countDown, 1000);
+
+// let count = 3;
+// const interval = setInterval(() => {
+//   If(count === 0) {
+//     console.log(‘blast off’)
+//     clearInterval(interval)
+//   } else {
+//     console.log(count)
+//     count–
+//   }
+// }, 1000)
