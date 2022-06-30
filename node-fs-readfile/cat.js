342077acb87ca8d04e662d1b6ff6783ab4d468e0
Chorong Shin <chorongstellashin@gmail.com>
2022-06-30 17:00:27 +0000

    Working on optional challenge.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -12,3 +12,26 @@ for (let index = 2; index < filename.length; index++) {
     console.log(read);
   });
 }
+
+// function processDoll(doll) {
+// 1) base case -stoping condition
+// if (found the piece of chocolate) {
+//   return'yum yum';
+// }
+// 2) base case fails, recursive call to itself
+// else if (there are no more dolls) {
+// return 'no chocolate here'
+// }
+// else
+// processDoll(the smaller doll)
+// }
+
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(4));
