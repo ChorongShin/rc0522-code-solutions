3ea66371434ca00888de5a3ca49a5f642bfcc2ed
Chorong Shin <chorongstellashin@gmail.com>
2022-07-11 17:43:27 +0000

    Edited some codes to look simple

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -86,33 +86,34 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
 });
 
 app.post('/api/grades', (req, res) => {
-  const name = req.body.name;
-  const course = req.body.course;
+  const { name, course } = req.body;
   const score = parseInt(req.body.score, 10);
 
-  if (!req.body.name) {
-    res.status(400).json({ error: 'Name is a required field' });
-    return;
-  } else if (!req.body.course) {
-    res.status(400).json({ error: 'Course is a required field' });
-    return;
-  } else if (!req.body.score) {
-    res.status(400).json({ error: 'Score is a required field' });
-    return;
-  } else if (!Number.isInteger(req.body.score) || req.body.score < 0 || req.body.score > 100) {
-    res.status(400).json({ error: 'Score has to be an integer from 0 to 100' });
+  if (!name || !course || !score) {
+    res.status(400).json({
+      error: '"name, course, score" are required fields'
+    });
     return;
   }
 
-  const text = 'insert into grades(name, course, score) values($1, $2, $3) returning *';
-  const values = [name, course, score];
+  if (score < 0) {
+    res.status(400).json({
+      error: 'Score must be a positive integer from 1 to 100'
+    });
+    return;
+  }
 
-  db.query(text, values)
+  const sql = `
+  insert into "grades" ("name", "course", "score")
+  values($1, $2, $3)
+  returning *`;
+
+  const params = [name, course, score];
+
+  db.query(sql, params)
     .then(result => {
-      const grade = result.rows[0];
-      // console.log('result:', result);
-      // console.log('result.rows:', result.rows);
-      // console.log('grade:', grade);
+      const [grade] = result.rows;
+      // const grade = result.rows[0]
       res.status(201).json(grade);
 
     })
@@ -126,62 +127,53 @@ app.post('/api/grades', (req, res) => {
 });
 
 app.put('/api/grades/:gradeId', (req, res, next) => {
-  const name = req.body.name;
-  const course = req.body.course;
-  const score = parseInt(req.body.score, 10);
   const gradeId = Number(req.params.gradeId);
 
-  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+  if (!Number.isInteger(gradeId) || gradeId < 0) {
     res.status(400).json({
       error: '"gradeId" must be positive integer'
     });
     return;
-  } else if (!req.body.name) {
+  }
+
+  const { name, course } = req.body;
+  const score = parseInt(req.body.score, 10);
+
+  if (!name || !course || !score) {
     res.status(400).json({
-      error: 'Name is a required field'
+      error: '"name, course, score" are required fields'
     });
     return;
-  } else if (!req.body.course) {
+  }
+
+  if (score < 0) {
     res.status(400).json({
-      error: 'Course is a required field'
+      error: 'Score must be a positive integer from 1 to 100'
     });
-    return;
-  } else if (!req.body.score) {
-    res.status(400).json({
-      error: 'Score is a required field'
-    });
-    return;
-  } else if (!Number.isInteger(req.body.score) || req.body.score < 0 || req.body.score > 100) {
-    res.status(400).json({ error: 'Score has to be an integer from 0 to 100' });
     return;
   }
 
   const sql = `
-          update grades
-          set "name" = $2,
-              "course" = $3,
-              "score" = $4
-          where "gradeId" = $1
+          update "grades"
+          set "name" = $1
+              "course" = $2,
+              "score" = $3
+          where "gradeId" = $4
           returning *`;
 
-  // const text = 'UPDATE grades(name, course, score) SET($1, $2, $3) WHERE gradeId = $4';
-  // const values = [name, course, score];
-  const params = [gradeId, name, course, score];
+  const params = [name, course, score, gradeId];
 
   db.query(sql, params)
     .then(result => {
       const grade = result.rows[0];
-      // console.log('Is the array empty?', result.rows.length === 0);
-      // console.log('result:', result);
-      // console.log('result.rows', result.rows);
-      // console.log('grade:', grade);
+      // const [grade] = result.rows
 
       if (!grade) {
         res.status(404).json({
           error: `Cannot find grade with "gradeId" ${gradeId}`
         });
       } else {
-        res.json(grade);
+        res.status(200).json(grade);
       }
     })
     .catch(err => {
@@ -194,12 +186,11 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
 });
 
 app.delete('/api/grades/:gradeId', (req, res) => {
-
   const gradeId = Number(req.params.gradeId);
 
   if (!Number.isInteger(gradeId) || gradeId < 0) {
     res.status(400).json({
-      error: '"gradeId" must be positive integer or invalid gradeId'
+      error: '"gradeId" must be positive integer'
     });
     return;
   }
@@ -209,11 +200,13 @@ app.delete('/api/grades/:gradeId', (req, res) => {
   where "gradeId" = $1
   returning *
   `;
+
   const params = [gradeId];
 
   db.query(sql, params)
     .then(result => {
       const grade = result.rows[0];
+      // const [grade] = result.rows
       if (!grade) {
         res.status(404).json({
           error: `Cannot find grade with "gradeId" ${gradeId}`
