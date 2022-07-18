5ab3d8c3a6d7f7f36bc5fc38139a3f4163339e8f
Chorong Shin <chorongstellashin@gmail.com>
2022-07-18 22:52:16 +0000

    Added class solution

--- express-static/index.js
+++ express-static/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Class Solution
+const publicPath = path.join(__dirname, 'public');
+console.log('public path:', publicPath);
+const staticMiddleware = express.static(publicPath);
+console.log('staticMidleware:', staticMiddleware);
+app.use(staticMiddleware);
 
 app.listen(3000, () => {
   console.log('Express server listening on port 3000');
